9e2c633915fa997ccde04419d5cca252e8ce1e70
capital-yinmonoo <august16895>
2024-07-17 15:20:02 +0630

    KSDT(GroupViewtableselect)

---
+++ TOSCore/Controllers/RegistrationApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Buffers.Text;
+using System.Data;
+using System.Xml.Linq;
+using TOSCore.Context;
+using TOSCore.Models;
+using TOSCore.Services;
+
+namespace TOSCore.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RegistrationApiController : ControllerBase
+    {
+        RegistrationService _service = new RegistrationService();
+        [EnableCors("_myAllowSpecificOrigins")]
+        [HttpGet("GetGroupView")]
+        public async Task<List<GroupModel>> GetGroupView()
+        {
+            return await _service.GetGroupView();
+        }
+    }
+}
---
+++ TOSCore/Services/RegistrationService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using System.Data;
+using TOSCore.Context;
+using TOSCore.Models;
+
+namespace TOSCore.Services
+{
+    public class RegistrationService
+    {
+        TOSContext _context = new TOSContext();
+        public async Task<List<GroupModel>> GetGroupView()
+        {
+            List<GroupModel> gpInfoList = new List<GroupModel>();
+            DbCommand cmd = _context.Database.GetDbConnection().CreateCommand(); ;
+            DbDataReader rdr;
+            string sql = "Group_View_Select";
+            cmd.CommandText = sql;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@id", SqlDbType.VarChar) { Value = DBNull.Value });
+            cmd.Parameters.Add(new SqlParameter("@option", SqlDbType.VarChar) { Value = 1 });
+            _context.Database.OpenConnection();
+            rdr = await cmd.ExecuteReaderAsync();
+            while (rdr.Read())
+            {
+                if (rdr.HasRows)
+                {
+                    gpInfoList.Add(new GroupModel
+                    {
+                        No = rdr["No"].ToString(),
+                        GroupID = rdr["GroupID"].ToString(),
+                        GroupName = rdr["GroupName"].ToString(),
+                        GroupDesignation = rdr["GroupDesignation"].ToString(),
+
+                    });
+                }
+            }
+            return gpInfoList;
+        }
+
+    }
+}
